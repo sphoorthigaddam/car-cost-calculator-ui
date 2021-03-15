ae36f78478e35bceb3432eb58a0975d9dcd7d055
Sphoorthi Gaddam <sphoorthigaddam@gmail.com>
2021-03-14 21:43:31 -0400

    Integrated Rest call to calculate cost api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import CalculateForm from './CalculateForm';
 import CostCalculator from './CostCalculator.js'
 
 function App() {
 	return (
 		<div className="App">
-			<CostCalculator />
+			<CalculateForm/>
 		</div>
 	);
 }
